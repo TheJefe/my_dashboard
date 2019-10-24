488e4bfbaa31c6aa2794b3e580413a45014ac35d
Jeff Koenig <jkoenig311@gmail.com>
2019-10-24 10:28:05 -0400

    Tune the calendar widget for when there are more events in the day

--- jobs/google_calendar.rb
+++ jobs/google_calendar.rb
@@ -9,6 +9,7 @@ require 'open-uri'
 # Example:
 #   hangouts: "https://www.google.com/calendar/ical/<hash>.calendar.google.com/private-<hash>/hangouts.ics"
 calendars = Hash[*File.read('calendars.config').split(/[, \n]+/)]
+max_events = 3
 
 SCHEDULER.every '5m', :first_in => 0 do |job|
 
@@ -31,7 +32,7 @@ SCHEDULER.every '5m', :first_in => 0 do |job|
     events = events.select{ |e| DateTime.parse(e.dtend.to_s) > Date.today && DateTime.parse(e.dtend.to_s) < Date.tomorrow }
 
     # sort by start time
-    events = events.sort{ |a, b| DateTime.parse(a.dtstart.to_s) <=> DateTime.parse(b.dtstart.to_s) }[0..4]
+    events = events.sort{ |a, b| DateTime.parse(a.dtstart.to_s) <=> DateTime.parse(b.dtstart.to_s) }[0..(max_events-1)]
 
 
     events = events.map do |e|
