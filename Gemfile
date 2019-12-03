5c4a2b553c3516d215939fe9a15c0a119edc6e18
Jeff Koenig <jkoenig311@gmail.com>
2019-12-03 09:08:33 -0500

    Use google calendar API instead of ical
    This is a LOT less buggy

--- Gemfile
+++ Gemfile
@@ -3,5 +3,6 @@ source 'https://rubygems.org'
 gem 'smashing'
 
 gem 'icalendar'
-gem 'google-api-client', '0.8'
+gem 'google-api-client', '0.34.1'
 gem 'octokit'
+gem 'pry'
--- jobs/google_calendar.rb
+++ jobs/google_calendar.rb
@@ -1,49 +1,72 @@
-require 'net/http'
-require 'icalendar'
-require 'open-uri'
+require "google/apis/calendar_v3"
+require "googleauth"
+require "googleauth/stores/file_token_store"
+require "fileutils"
 
-# List of calendars
-#
-# Format:
-#   <name> => <uri>
-# Example:
-#   hangouts: "https://www.google.com/calendar/ical/<hash>.calendar.google.com/private-<hash>/hangouts.ics"
-calendars = Hash[*File.read('calendars.config').split(/[, \n]+/)]
+# Src: https://developers.google.com/calendar/quickstart/ruby
+
+calendars = Hash[*File.read('gcal.config').split(/[, \n]+/)]
 max_events = 3
 
+OOB_URI = "urn:ietf:wg:oauth:2.0:oob".freeze
+APPLICATION_NAME = "Dashing Personal Calendar".freeze
+CREDENTIALS_PATH = "credentials.json".freeze
+# The file token.yaml stores the user's access and refresh tokens, and is
+# created automatically when the authorization flow completes for the first
+# time.
+TOKEN_PATH = "token.yaml".freeze
+SCOPE = Google::Apis::CalendarV3::AUTH_CALENDAR_READONLY
+
+##
+# Ensure valid credentials, either by restoring from the saved credentials
+# files or intitiating an OAuth2 authorization. If authorization is required,
+# the user's default browser will be launched to approve the request.
+#
+# @return [Google::Auth::UserRefreshCredentials] OAuth2 credentials
+def authorize
+  client_id = Google::Auth::ClientId.from_file CREDENTIALS_PATH
+  token_store = Google::Auth::Stores::FileTokenStore.new file: TOKEN_PATH
+  authorizer = Google::Auth::UserAuthorizer.new client_id, SCOPE, token_store
+  user_id = "default"
+  credentials = authorizer.get_credentials user_id
+  if credentials.nil?
+    url = authorizer.get_authorization_url base_url: OOB_URI
+    puts "Open the following URL in the browser and enter the " \
+         "resulting code after authorization:\n" + url
+    code = gets
+    credentials = authorizer.get_and_store_credentials_from_code(
+      user_id: user_id, code: code, base_url: OOB_URI
+    )
+  end
+  credentials
+end
+
+def init
+  # Initialize the API
+  service = Google::Apis::CalendarV3::CalendarService.new
+  service.client_options.application_name = APPLICATION_NAME
+  service.authorization = authorize
+  return service
+end
+
 SCHEDULER.every '5m', :first_in => 0 do |job|
-
-  calendars.each do |cal_name, cal_uri|
-
-    ics  = open(cal_uri) { |f| f.read }
-    cal = Icalendar::Calendar.parse(ics).first
-    events = cal.events
-    # events = events.first(100)
-
-    # select only current and upcoming events
-    now = DateTime.now.utc
-
-    events = events.select  { |event| event.dtend.class == Icalendar::Values::DateTime  }
-    events = events.select  { |event| event.dtstart.class == Icalendar::Values::DateTime  }
-
-    events = events.select{ |e| DateTime.parse(e.dtend.to_s) > now }
-
-    # select events for today
-    events = events.select{ |e| DateTime.parse(e.dtend.to_s) > Date.today && DateTime.parse(e.dtend.to_s) < Date.tomorrow }
-
-    # sort by start time
-    events = events.sort{ |a, b| DateTime.parse(a.dtstart.to_s) <=> DateTime.parse(b.dtstart.to_s) }[0..(max_events-1)]
-
-
-    events = events.map do |e|
+  service = init()
+  calendars.each do |cal_name, calendar_id|
+    response = service.list_events(
+      calendar_id,
+      max_results:   max_events,
+      single_events: true,
+      order_by:      "startTime",
+      time_min:      DateTime.now.rfc3339
+    )
+    events = response.items.each.map do |event|
       {
-        title: e.summary,
-        start: DateTime.parse(e.dtstart.to_s).to_i,
-        end: DateTime.parse(e.dtend.to_s).to_i
+        title: event.summary,
+        start: event.start.date || event.start.date_time.to_time.to_i,
+        end: event.end.date || event.end.date_time.to_s
       }
     end
-    puts "**************#{events.count}***************"
+    puts "=====#{cal_name}======"
     send_event("google_calendar_#{cal_name}", {events: events, cal_name: cal_name})
   end
-
 end
